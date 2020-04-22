3aab941f9103cf626a0c05af8e43b0524c83e0ef
Jamie <phillipsja97@gmail.com>
2020-04-22 13:55:07 -0500

    added a material UI card

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import './App.scss';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { withStyles } from "@material-ui/core/styles";
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import LoginButton from './Components/Shared/LoginButtonScreen/LoginButtonScreen';
 
 const spotifyApi = new SpotifyWebApi();
+
+
+const styles = theme => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
 
 class App extends Component {
   constructor(){
@@ -32,12 +62,37 @@ class App extends Component {
   }
 
   render () {
+    const { classes } = this.props;
 return (
     <div className="App">
-      { this.state.loggedIn ? <h1>Thanks for logging in!</h1> : <a href='http://localhost:8888'> Login to Spotify </a>}
+     <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <LoginButton />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+      </CardActions>
+    </Card>
     </div>
   );
   }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
