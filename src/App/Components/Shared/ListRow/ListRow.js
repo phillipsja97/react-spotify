13f2267fb90e1cfe5923f047c206837dbdf799a0
Jamie <phillipsja97@gmail.com>
2020-04-24 21:23:08 -0500

    data works. playlist names and images render

---
+++ src/App/Components/Shared/ListRow/ListRow.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    color: '#01FFC3'
+  },
+}));
+
+const ListRow = props => {
+  const [playlist, setPlaylist] = useState(props);
+  const classes = useStyles();
+
+  useEffect(() => {
+    setPlaylist(props);
+    console.log(playlist);
+  }, [props]);
+
+  return (
+    <ListItem>
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" variant="square" src={playlist.playlist.images[0].url} />
+          </ListItemAvatar>
+        <ListItemText className={classes.text} primary={playlist.playlist.name} secondary="Jan 9, 2014" />
+      </ListItem>
+  )
+}
+
+export default ListRow;
--- src/App/Components/Shared/UserCard/UserCard.js
+++ src/App/Components/Shared/UserCard/UserCard.js
@@ -1,51 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { FixedSizeList } from 'react-window';
+import List from '@material-ui/core/List';
+import ListRow from '../ListRow/ListRow';
 import spotifyWebApi from 'spotify-web-api-js';
+import './UserCard.scss';
+
 
 const spotifyApi = new spotifyWebApi();
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '100%',
+    maxWidth: '50vw',
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-function renderRow(props) {
-  const { index, style } = props;
-
-  return (
-    <ListItem button style={style} key={index}>
-      <ListItemText primary={`Item ${index + 1}`} />
-    </ListItem>
-  );
-}
-
-renderRow.propTypes = {
-  index: PropTypes.number.isRequired,
-  style: PropTypes.object.isRequired,
-};
-
-export default function Playlists() {
+export default function FolderList() {
   const classes = useStyles();
-  const [playlists, setPlaylists] = useState([])
+    const [playlists, setPlaylists] = useState({})
 
   useEffect(() => {
     spotifyApi.getUserPlaylists()
-    .then(response => response.items).then(playlists => setPlaylists(playlists));
+    .then((response) => {
+      setPlaylists(response);
+    })
     console.log(playlists, "playlists");
   }, []);
 
   return (
-    <div className={classes.root}>
-      <FixedSizeList height={700} width={'100%'} itemSize={46} itemCount={200}>
-        {renderRow}
-      </FixedSizeList>
+    <div className="userPlaylists">
+      <List>
+        { (playlists.items) ? playlists.items.map((playlist) => <ListRow key={playlist.id} playlist={playlist} />) : null }
+      </List>
     </div>
   );
 }
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { makeStyles } from '@material-ui/core/styles';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import { FixedSizeList } from 'react-window';
+// import spotifyWebApi from 'spotify-web-api-js';
+// import RenderRow from '../ListRow/ListRow';
+
+// const spotifyApi = new spotifyWebApi();
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//     maxWidth: '100%',
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// }));
+
+// export default function Playlists() {
+//   const classes = useStyles();
+//   const [playlists, setPlaylists] = useState([])
+
+//   useEffect(() => {
+//     spotifyApi.getUserPlaylists()
+//     .then((response) => {
+//       setPlaylists(playlists => [...playlists, response.items])
+//     })
+//     console.log(playlists, "playlists");
+//   }, [playlists[0]]);
+
+//   return (
+//     <div className={classes.root}>
+//       <FixedSizeList height={700} width={'100%'} itemSize={46} itemCount={200}>
+//         { playlists.map((playlist) => <RenderRow key={playlist.id} playlist={playlist} />)}
+//       </FixedSizeList>
+//     </div>
+//   );
+// }
